a342ac8f07e24ec6633685166a63ae13c684ec3e
bazarkinzakhar <bazarkinzakhar@gmail.com>
2026-04-19 21:13:07 +0300

    cleanup: move homelab files out of scripts root

--- init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir -p data/prometheus data/grafana data/html data/jellyfin/config data/jellyfin/cache data/media
-chmod -R 777 data/
-
-echo "✅ Структура папок готова. Можно запускать docker compose up -d"
